ed6380678fcd09c2ccd02ca3c2f605a598356741
John Brown <echonode1@gmail.com>
2026-05-14 09:52:21 -0400

    removed useless print statements from build-helper.ps1

--- helper/build-helper.ps1
+++ helper/build-helper.ps1
@@ -3,10 +3,7 @@ param(
     [string]$OutputFile
 )
 
-Write-Host "[BUILD] Starting Red compilation..."
-
 $redCompiler = "redc"
-
 
 # I fucking guess $args is reserved in powershell, so we have to use a different variable name for the arguments array. What the fuck.
 $args1 = @(
@@ -23,8 +20,5 @@ $process = Start-Process `
     -PassThru
 
 if ($process.ExitCode -ne 0) {
-    Write-Host "[ERROR] Compilation failed."
     exit $process.ExitCode
 }
-
-Write-Host "[BUILD] Compilation finished successfully."
